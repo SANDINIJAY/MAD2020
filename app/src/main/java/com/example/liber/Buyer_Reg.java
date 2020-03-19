fa4bb7c4a5f52b15e25604cacaad1edb504d4634
Sam584 <61457411+Sam584@users.noreply.github.com>
2020-03-19 02:57:29 -0700

    Hello

---
+++ app/src/main/java/com/example/liber/Buyer_Reg.java
@@ -0,0 +1,14 @@
+package com.example.liber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buyer_Reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer__reg);
+    }
+}
---
+++ app/src/main/java/com/example/liber/Mobile_Num.java
@@ -0,0 +1,14 @@
+package com.example.liber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mobile_Num extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile__num);
+    }
+}
