d9f3514f9b92c3ae8c14f79f51f28590bbaca3e1
SANDINIJAY <sandinijay@gmail.com>
2020-05-01 10:51:13 +0530

    Item delete function

--- app/src/main/java/com/example/mad20/Itemadapter.java
+++ app/src/main/java/com/example/mad20/Itemadapter.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -31,10 +37,18 @@ public class Itemadapter extends RecyclerView.Adapter<Itemadapter.MyViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        DataItem dataItems=this.dataItems.get(position);
+        final DataItem dataItems=this.dataItems.get(position);
         holder.Itemname.setText(dataItems.getItemname());
         holder.Price.setText(dataItems.getPrice());
         holder.Quantity.setText(dataItems.getQuantity());
+        holder.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("DataItem").child(dataItems.getItemname());
+                databaseReference.removeValue();
+                Toast.makeText(context,"Item Deleted",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,11 +58,16 @@ public class Itemadapter extends RecyclerView.Adapter<Itemadapter.MyViewHolder> 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView Itemname,Price,Quantity;
+        Button button5;
+        CardView cardView;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             Itemname=itemView.findViewById(R.id.Itemname);
             Price=itemView.findViewById(R.id.Price);
             Quantity=itemView.findViewById(R.id.Quantity);
+            cardView = itemView.findViewById(R.id.cardView);
+            button5=itemView.findViewById(R.id.button5);
         }
     }
 }
