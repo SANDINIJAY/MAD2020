5196d4e549da307b22720065bb932f1999b06d68
Sam584 <61457411+Sam584@users.noreply.github.com>
2020-04-30 19:23:31 -0700

    Liber

--- app/src/main/java/com/example/liber/Buyer_Login.java
+++ app/src/main/java/com/example/liber/Buyer_Login.java
@@ -1,17 +1,22 @@
 package com.example.liber;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
@@ -20,6 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Buyer_Login extends AppCompatActivity {
 
      EditText  bEmail, bPassword;
+     TextView forotPassword;
      Button bLogin;
      FirebaseAuth mAuth;
 
@@ -32,6 +38,7 @@ public class Buyer_Login extends AppCompatActivity {
         bEmail = findViewById(R.id.b_Email);
         bPassword = findViewById(R.id.b_Password);
         bLogin = findViewById(R.id.b_Login);
+        forotPassword = findViewById(R.id.frogotPassword);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -78,6 +85,48 @@ public class Buyer_Login extends AppCompatActivity {
                startActivity(new Intent(getApplicationContext(),Buyer_Reg.class));
            }
                 });
+
+       forotPassword.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               EditText restMail = new EditText(v.getContext());
+               final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+               passwordResetDialog.setTitle("Reset Password?");
+               passwordResetDialog.setMessage("Enter your Email To Received Reset Link");
+               passwordResetDialog.setView(restMail);
+
+               passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       String mail = resetMail.getText().toString();
+                       mAuth.sendPasswordResetEmail(mail);addOnSuccessListener(new OnSuccessListener<Void>() {
+                           @Override
+                           public void onSuccess(Void aVoid) {
+
+                           }
+                       });{
+
+                       }
+                       @Override
+                           public void OnSuccess(Void ){
+                               Toast.makeText(Buyer_Login.this,"Reset Link Sent To Your Email",Toast.LENGTH_SHORT).show();
+                           });addFailureListener(new OnFailureListener(){
+                               @Override
+                                public void onFailure(@NonNull Exception e){
+                                   Toast.makeText(Buyer_Login.this,"Error Reset Link is Not sent"+e.getMessage() Toast.LENGTH_SHORT).show();)
+                               }
+                           });
+                       }
+
+               });
+                passwordResetDialog.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                            public void onClick(DialogInterface dialog, int which){
+
+                       }
+                   }
+           }
+       });
             }
 
         }
